1649820b6ec33218f78beb3726b78945146c9a9c
YhomiAce <kareemyomi91@gmail.com>
2023-03-06 10:41:09 +0100

    get user test case

--- test/Auth.test.js
+++ test/Auth.test.js
@@ -43,19 +43,19 @@ describe("Test Authentication usecases", () => {
     });
   });
 
-  describe("Given correct parameters", () => {
-    // should respond with status code of 201
-    test("should respond with a status code of 201", async () => {
-      const response = await request(app)
-        .post("/api/auth/register")
-        .send({
-          email: "test10@test.com",
-          password: "123456",
-          name: "Test Ten"
-        });
-      expect(response.statusCode).toBe(201);
-    });
-  });
+  //   describe("Given correct parameters", () => {
+  //     // should respond with status code of 201
+  //     test("should respond with a status code of 201", async () => {
+  //       const response = await request(app)
+  //         .post("/api/auth/register")
+  //         .send({
+  //           email: "test10@test.com",
+  //           password: "123456",
+  //           name: "Test Ten"
+  //         });
+  //       expect(response.statusCode).toBe(201);
+  //     });
+  //   });
 
   // Login test cases
   describe("when email and password is missing", () => {
@@ -93,6 +93,26 @@ describe("Test Authentication usecases", () => {
           email: "test4@test.com",
           password: "12345"
         });
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  // Get user test case
+  describe("Given wrong authorization header or no authoraization header", () => {
+    // should respond with status code of 401
+    test("should respond with a status code of 401", async () => {
+      const response = await request(app).get("/api/auth/me");
+      expect(response.statusCode).toBe(401);
+    });
+  });
+  describe("Given authorization header", () => {
+    // should respond with status code of 200
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwM2JlOWQ1YjU4NWI0M2ZlZjM3YTEwIn0sImlhdCI6MTY3ODA5NTQzOCwiZXhwIjoxNjc4MTMxNDM4fQ.clKgj8WHYT_Lepf1EeWYmCg6trkTzvsNIALYyV9rt6I";
+    test("should respond with a status code of 200", async () => {
+      const response = await request(app)
+        .get("/api/auth/me")
+        .set("Authorization", token);
       expect(response.statusCode).toBe(200);
     });
   });
